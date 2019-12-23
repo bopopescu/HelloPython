e5a3f77b6d86af34bbeea8bf6a2544e7b8e721ae
pipipao <pipipao@users.noreply.github.com>
2019-12-23 20:45:50 +0800

    Create balls_2.py

---
+++ venv/src/Python100days/day6_12_23/balls_2.py
@@ -0,0 +1,24 @@
+from enum import Enum,unique
+from math import sqrt
+from random import randint
+
+import pygame
+
+@unique
+class Color(Enum):
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    GRAY = (242, 242, 242)
+
+    @staticmethod
+    def random_color():
+        r = randint(0, 255)
+        g = randint(0, 255)
+        b = randint(0, 255)
+        return (r, g, b)
+
+class Ball(object):
+    def __init__(self,):
